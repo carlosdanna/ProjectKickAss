1f3e23a0b863d6b30d4124d77dfc99a3d2abe100
OzzieOsborn01 <andrewso.saxophone@rocketmail.com>
2015-01-24 00:16:50 -0500

    Preliminary Credits
    credits

---
+++ GGJ2015/Assets/Scripts/Menu Scripts/CreditsScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour {
+
+	public float resetPoint = -574;
+	public float beginPoint = 228;
+	public float y;
+	public float currentPoint;
+	public float scrollVelocity;
+	public GameObject mainMenu;
+	public GameObject creditsMenu;
+	public GameObject text;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 vec = Vector3.zero;
+		currentPoint += Time.deltaTime * scrollVelocity;
+
+		vec.y = Time.deltaTime * scrollVelocity;
+		text.transform.Translate(vec);
+
+		if(currentPoint <= resetPoint)
+		{
+			BacktoMainMenu();
+		}
+	
+	}
+
+	void BacktoMainMenu()
+	{
+		currentPoint = beginPoint;
+		mainMenu.SetActive(true);
+		creditsMenu.SetActive(false);
+	}
+}
