2041b353d0f373f067b4da7c583b38e619f64116
carlosdanna <carlos.danna@gmail.com>
2015-01-24 14:40:31 -0500

    Level Design 2

---
+++ GGJ2015/Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public SpriteRenderer Level;
+    public float limitLeft;
+    public float limitRight;
+
+	// Use this for initialization
+	void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
