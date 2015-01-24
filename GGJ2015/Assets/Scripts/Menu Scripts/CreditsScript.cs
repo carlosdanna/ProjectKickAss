02b4b5f4474198608c686040acc0710aaff24e1b
OzzieOsborn01 <andrewso.saxophone@rocketmail.com>
2015-01-24 12:29:42 -0500

    Credits and Main Menu
    Credits and Main Menu combined - could use polish

--- GGJ2015/Assets/Scripts/Menu Scripts/CreditsScript.cs
+++ GGJ2015/Assets/Scripts/Menu Scripts/CreditsScript.cs
@@ -10,30 +10,28 @@ public class CreditsScript : MonoBehaviour {
 	public float scrollVelocity;
 	public GameObject mainMenu;
 	public GameObject creditsMenu;
-	public GameObject text;
+	public Transform transform;
 
 	// Update is called once per frame
-	void Update ()
+	public void Update ()
 	{
+		float test = transform.localPosition.y;
 		Vector3 vec = Vector3.zero;
-		currentPoint += Time.deltaTime * scrollVelocity;
-
 		vec.y = Time.deltaTime * scrollVelocity;
-		text.transform.Translate(vec);
+		transform.Translate(vec);
 
-		if (text.transform.position.y >= resetPoint)
+		if (transform.localPosition.y >= resetPoint)
 		{
 			BacktoMainMenu();
 		}
 	
 	}
 
-	void BacktoMainMenu()
+	public void BacktoMainMenu()
 	{
-		currentPoint = 0;
 		Vector3 vec = Vector3.zero;
-		vec.y = -(resetPoint - beginPoint + (text.transform.position.y-resetPoint));
-		text.transform.Translate(vec);
+		vec.y = -(transform.localPosition.y - beginPoint);
+		transform.Translate(vec);
 		mainMenu.SetActive(true);
 		creditsMenu.SetActive(false);
 	}
