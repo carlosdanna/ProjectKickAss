c0ab29c9e054b524e55a9937ead8f921056c62a8
Jehanlos Roman <me@jehanlos.com>
2015-01-24 16:21:31 -0500

    Switch between Main Menu and Level. Changed Timer font to pixelated font.

---
+++ GGJ2015/Assets/Scripts/Menu Scripts/PlayButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void GotoCredits()
+	{
+        Application.LoadLevel("Level");
+	}
+}
